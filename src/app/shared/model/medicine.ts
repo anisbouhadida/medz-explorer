d65cb5006c77961c3f58d693ea665add6c18cc2b
anisbouhadida <anisdoescode@outlook.fr>
2026-04-19 10:03:00 +0200

    feat: add Medicine model and service with basic structure and tests

---
+++ src/app/shared/model/medicine.ts
@@ -0,0 +1,14 @@
+export interface Medicine {
+  registrationNumber: string;
+  code: string;
+  icd: string;
+  brandName: string;
+  form: string;
+  dosage: string;
+  packaging: string;
+  list: string;
+  availableHospital: boolean;
+  availablePharmacy: boolean;
+  laboratoryHolder: string;
+  laboratoryCountry: string;
+}
